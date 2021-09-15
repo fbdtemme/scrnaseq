4e08759c224e160a5d0e98d05fb83c6555b9cfda
jfnavarro <jc.fernandez.navarro@gmail.com>
2021-09-15 15:20:41 +0200

    Add support for QC plots and ensembl ids conversion in postprocess

--- bin/postprocessing.py
+++ bin/postprocessing.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 """
-This script parses a single cell dataset in MTX format to dataframe (tab delimited) and hd5 formats. 
+This script parses a single cell dataset in MTX format to dataframe (tab delimited), loompy and hd5 formats. 
 The script will also place all the output files as well as the orignal MTX files in a output directory
 given by the user. The script is useful to parse the output of tools such as STARSolo, Kallisto bustools
-and Salmon Alevin. 
+and Salmon Alevin. A set of basic QC plots will also be generated in the output folder. Users can use
+an option to convert Ensembl ids to gene names if needed. 
+
+Type:
+
+postprocessing.py --help
+
+To get extra information on the input parameters. 
 
 How to use it:
 
-python postprocessing.py --input data.mtx.gz --features features.txt --barcodes barcodes.txt --output out 
+postprocessing.py --input data.mtx.gz --features features.txt --barcodes barcodes.txt --output out 
 
 After this you should find in the out directory the input matrix in the original format as well as the
-other formats. The input matrix in MTX format must be gzipped. 
+other formats. The QC figures will be placed in a subfolder called figures.  
 
 Author: Jose Fernandez Navarro (jfnavarro) <jc.fernandez.navarro@gmail.com>
 """
@@ -22,9 +29,11 @@ import scipy.io
 import shutil
 import pandas as pd
 import scanpy as sc
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-def main(matrix_file, features_file, barcodes_file, transpose, output):
+def main(matrix_file, features_file, barcodes_file, transpose, gene_names, organism, output):
 
 	if not os.path.isfile(matrix_file):
 		sys.stderr.write("Error, input matrix {} is not a file\n".format(matrix_file))
@@ -37,6 +46,12 @@ def main(matrix_file, features_file, barcodes_file, transpose, output):
 	if not os.path.isfile(barcodes_file):
 		sys.stderr.write("Error, input barcodes {} is not a file\n".format(barcodes_file))
 		sys.exit(1)
+	
+	if (organism is not None and not gene_names) or (gene_names and organism is None):
+		sys.stderr.write("Error, --organism must be used together with --gene-names\n")
+		sys.exit(1)
+
+	# TODO check that organism is valid if gene_names is True
 
 	if os.path.isdir(output):
 		print("Ouput directory {} exists, files will be overwritten..".format(output))
@@ -55,36 +70,64 @@ def main(matrix_file, features_file, barcodes_file, transpose, output):
 	df = pd.DataFrame(matrix.toarray().transpose() if transpose else matrix.toarray(), 
 	                  index=features, columns=barcodes)
 
-    # Write Pandas
+    # Convert Ensembl ids to gene names
+	if gene_names:
+		annot = sc.queries.biomart_annotations(
+        	organism, ["ensembl_gene_id", "external_gene_name", "chromosome_name"],
+    	).set_index("ensembl_gene_id")
+		df.index = [x.split('.')[0] for x in df.index]
+		intersect = np.intersect1d(df.index, annot.index)
+		df = df.loc[intersect, :]
+		df.index = annot.loc[intersect, 'external_gene_name']	
+
+	# Rename rows/columns
+	df.index = df.index.rename("Genes")
+	df.columns = df.columns.rename("Barcodes")
+
+	# Create Anndata and rename duplicates (if any)
+	adata = sc.AnnData(df.transpose())
+	adata.var_names_make_unique()
+	adata.obs_names_make_unique()
+	df.index = adata.var.index
+	df.columns = adata.obs.index
+
+	# Write Pandas
 	df.to_csv(os.path.join(output, 'matrix.tsv'), sep='\t', header=True, index=True)
 
-    # Write hdf5
+	# Write hdf5
 	df.to_hdf(os.path.join(output, 'matrix.h5'), key='df', mode='w')
 
-    # Write mtx
-	shutil.copy(matrix_file, os.path.join(output, 'matrix.mtx.gz' if matrix_file.endswith('gz') else 'matrix.mtx'))
+	# Write mtx
+	extension = os.path.splitext(matrix_file)[1] if not matrix_file.endswith('.mtx') else ''
+	shutil.copy(matrix_file, os.path.join(output, 'matrix.mtx{}'.format(extension)))
 	shutil.copy(features_file, os.path.join(output, 'features.mtx.txt'))
 	shutil.copy(barcodes_file, os.path.join(output, 'barcodes.mtx.txt'))
 
-	# Create AnnData object
-	adata = sc.read(os.path.join(output, 'matrix.tsv'), delimiter='\t')
-
-    # Write h5ad
+	# Write h5ad
 	adata.write(os.path.join(output, 'matrix.h5ad'))
 
-	# Write loom (TOFIX loompy is not insalled in the scanpy container)
-	#adata.write_loom(os.path.join(output, 'matrix.loom'))
+	# Write loom
+	adata.write_loom(os.path.join(output, 'matrix.loom'))
 
-    # Some basic QC and filtering
-	#sc.pl.highest_expr_genes(adata, n_top=20, )
-    #sc.pp.filter_cells(adata, min_genes=200)
-    #sc.pp.filter_genes(adata, min_cells=3)
-	#adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-    #sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+	# Some basic QC plots
+	sc.settings.figdir = os.path.join(output, sc.settings.figdir)
+	sc.set_figure_params(dpi=100, color_map='viridis_r')
+	sc.settings.authoshow = False
+	sc.settings.autosave = True
+	sc.pl.highest_expr_genes(adata, n_top=20)
+	adata.var['mt'] = adata.var_names.str.startswith('MT-')
+	sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+	sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], jitter=0.4, multi_panel=True)
+	sc.pl.scatter(adata, x='total_counts', y='pct_counts_mt', save='_mt.pdf')
+	sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts', save='_genes.pdf')
+	fig, axs = plt.subplots(1, 2, figsize=(15, 4))
+	sns.distplot(adata.obs["total_counts"], kde=False, ax=axs[0])
+	sns.distplot(adata.obs["n_genes_by_counts"], kde=False, bins=60, ax=axs[1])
+	fig.savefig(os.path.join(sc.settings.figdir, 'distplot.pdf'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True, 
-    	description='This tool parses a matrix of counts in mtx format to other formats (hdf5 and tsv)')
+    	description='This tool parses a matrix of counts in mtx format to other formats (hdf5, loompy and tsv)')
     parser.add_argument('--matrix', 
     	metavar="[FILE]", 
     	required=True,
@@ -92,19 +135,28 @@ if __name__ == "__main__":
     parser.add_argument('--features',
     	metavar="[FILE]",
     	required=True,
-    	help='Path to the column names (features) in text format')
+    	help='Path to the column names (features) in tab delimited text format')
     parser.add_argument('--barcodes',
     	metavar="[FILE]",
     	required=True,
-    	help='Path to the row names (barcodes) in text format')
+    	help='Path to the row names (barcodes) in tab delimited text format')
     parser.add_argument('--transpose',
 	    action="store_true",
 		default=False,
     	help='Use this if the input matrix has genes as columns and barcodes as rows')
+    parser.add_argument('--gene-names',
+	    action="store_true",
+		default=False,
+    	help='Use this if you want to convert the Ensembl ids to gene names')
+    parser.add_argument('--organism',
+    	metavar="[STR]",
+    	required=False,
+		default=None,
+    	help='BioMart organism to use when using --gene-names. Options are: hsapiens, mmusculus, drerio, etc..')
     parser.add_argument('--output',
     	metavar="[STRING]",
     	required=True,
     	help='Path to the output folder')
     
     args = parser.parse_args()
-    main(args.matrix, args.features, args.barcodes, args.transpose, args.output)
+    main(args.matrix, args.features, args.barcodes, args.transpose, args.gene_names, args.organism, args.output)
